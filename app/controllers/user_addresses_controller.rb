1c3689180aedfcfbfc0edbeb1b50941edd41812d
Zach <shaw.zacht@gmail.com>
2022-09-06 23:00:04 -0700

    add and update user addresses

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,2 +1,20 @@
 class UserAddressesController < ApplicationController
+
+    def update 
+        user = find_user
+        user.user_address.update!(user_address_params)
+        render json: user, status: :ok
+    end
+
+    def create
+        user = find_user
+        user.user_address.create!(user_address_params)
+        render json: user, status: :ok
+    end
+
+    private
+
+    def user_address_params
+        params.permit(:street, :city, :state, :zip)
+    end
 end
---
+++ client/src/components/AddressForm.js
@@ -0,0 +1,100 @@
+import { useContext, useState } from 'react'
+import { UserContext } from '../context/user'
+import { CartContext } from '../context/cart'
+
+
+function AddressForm({editAddress, setEditAddress}) {
+
+    const { user, setUser } = useContext(UserContext)
+    const { setCart } = useContext(CartContext)
+
+    const [street, setStreet] = useState("")
+    const [city, setCity] = useState("")
+    const [state, setState] = useState("")
+    const [zip, setZip] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [errors, setErrors] = useState([])
+
+    console.log(user.user_address.id)
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLoading(true);
+        fetch(`/user_addresses/${user.user_address.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                street: street, 
+                city: city,
+                state: state,
+                zip: zip
+            })
+        }).then((res) => {
+            if (res.ok) {
+                res.json().then((user) => {
+                    setUser(user);
+                    setEditAddress(false)
+                });
+            } else {
+                res.json().then((data) => setErrors(data.errors));
+            }
+        })
+    }
+
+
+
+
+    return (
+        <div>
+            <h1>{editAddress ? "Edit Address" : "Add Address"}</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="street">Street: </label>
+                <input 
+                    type="text"
+                    id="street"
+                    autoComplete="off"
+                    value={street}
+                    onChange={(e) => setStreet(e.target.value)}
+                    required
+                />        
+                <br />
+                <label htmlFor="city">City: </label>
+                <input 
+                    type="city"
+                    id="city"
+                    autoComplete="off"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                />
+                <br />
+                <label htmlFor="state">State: </label>
+                <input 
+                    type="state"
+                    id="state"
+                    autoComplete="off"
+                    value={state}
+                    onChange={(e) => setState(e.target.value)}
+                    required
+                />
+                <br />
+                <label htmlFor="zip">Zip: </label>
+                <input 
+                    type="zip"
+                    id="zip"
+                    autoComplete="off"
+                    value={zip}
+                    onChange={(e) => setZip(e.target.value)}
+                    required
+                />
+                <br />
+                <button type='submit'> {isLoading ? "Loading..." : "Submit Changes"} </button>
+                {errors ? errors.map((err) => <p key={err}>{err}</p>) : null}
+            </form>
+        </div>
+    )
+}
+
+export default AddressForm
