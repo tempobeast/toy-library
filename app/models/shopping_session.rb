9dea934e92113cbe26b7e76c5843c5988d8320d1
Zach <shaw.zacht@gmail.com>
2022-09-12 15:31:59 -0700

    updated_at formatting

--- app/models/shopping_session.rb
+++ app/models/shopping_session.rb
@@ -14,7 +14,11 @@ class ShoppingSession < ApplicationRecord
         self.cart_items.sum {|item| item.quantity}
     end
 
-    
+    def last_update
+        date = self.updated_at.to_s
+        split_date = date[0..9].split("-").rotate(1).join("-")
+        
+    end
 
     belongs_to :user
     has_many :cart_items, dependent: :destroy
--- app/serializers/shopping_session_serializer.rb
+++ app/serializers/shopping_session_serializer.rb
@@ -1,5 +1,5 @@
 class ShoppingSessionSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :status, :total_price, :total_items, :cart_items
+  attributes :id, :user_id, :status, :total_price, :total_items, :cart_items, :last_update
 
   has_many :cart_items
   
