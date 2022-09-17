835c52d30e04b0dbc1e57f3d4b1b8552923870dd
Zach <shaw.zacht@gmail.com>
2022-09-17 13:58:18 -0700

    namespace routes

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "bootsnap", require: false
 
 gem 'figaro'
 
+
 group :development, :test do
   gem 'listen', '~> 3.3'
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-  resources :user_addresses, only: [:update, :create]
-  resources :cart_items, only: [:create]
-  resources :shopping_sessions, only: [:create, :index, :update]
-  delete '/cancel_shopping_session/:id', to: 'shopping_sessions#cancel'
+  namespace :api do
+    resources :user_addresses, only: [:update, :create]
+    resources :cart_items, only: [:create]
+    resources :shopping_sessions, only: [:create, :index, :update]
+    delete '/cancel_shopping_session/:id', to: 'shopping_sessions#cancel'
 
-  resources :watch_lists, only: [:create, :destroy]
-  resources :toys, only: [:index, :create, :show]
-  resources :users
+    resources :watch_lists, only: [:create, :destroy]
+    resources :toys, only: [:index, :create, :show]
+    resources :users
 
 
-  get '/me', to: 'users#show'
-  post '/signup', to: 'users#create'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    get '/me', to: 'users#show'
+    post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
 
   get '*path',
     to: 'fallback#index',
