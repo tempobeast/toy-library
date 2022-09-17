5e8e6ceec1bdaa7f66b3528028e876b1a2e798b0
Zach <shaw.zacht@gmail.com>
2022-09-17 14:07:02 -0700

    heroku debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,15 +75,6 @@ Rails.application.configure do
   # host = 'localhost:3000'
   # config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
 
   config.action_mailer.delivery_method = :sendmail
   # Defaults to:
