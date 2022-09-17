0b516029a822d6eec80afbb7af1dcc652d71de35
Zach <shaw.zacht@gmail.com>
2022-09-17 14:00:34 -0700

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     post '/signup', to: 'users#create'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
+  end
 
   get '*path',
     to: 'fallback#index',
