cb1d573447bf9cd7f75c02127730b6bd635d5621
Zach <shaw.zacht@gmail.com>
2022-09-26 11:38:00 -0700

    edit typo for conditional text

--- app/mailers/order_details_mailer.rb
+++ app/mailers/order_details_mailer.rb
@@ -4,7 +4,7 @@ class OrderDetailsMailer < ApplicationMailer
         @user = params[:user]
         @user_address = "#{@user.user_address.street}, #{@user.user_address.city}, #{@user.user_address.state} #{@user.user_address.zip}"
         @order = params[:order]
-        if @total_items = 1
+        if @total_items == 1
             @total_items = "1 item"
         else
             @total_items = "#{@order.cart_items.length} items"
