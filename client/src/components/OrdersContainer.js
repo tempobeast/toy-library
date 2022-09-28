1f89d732901b0513972797eeef7f85a19351bfd9
Zach <shaw.zacht@gmail.com>
2022-09-28 12:18:17 -0700

    filter orders by status

--- client/src/components/OrdersContainer.js
+++ client/src/components/OrdersContainer.js
@@ -1,14 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PreviousOrdersCard from "./PreviousOrderCard";
 import { PreviousOrdersContext } from "../context/previousOrders";
 
 function OrdersContainer() {
   const { previousOrders } = useContext(PreviousOrdersContext);
+  const [ filterStatus, setFilterStatus ] = useState("all")
+
+  const filterOrders = previousOrders.filter((order) => {
+    if (filterStatus === "all") {
+      return true
+    } else {
+      return order.status === filterStatus
+    }
+  })
 
   return (
     <div>
       <h1>Orders</h1>
-      {previousOrders.map((order) => (
+      <label>filter by status:
+        <select onChange={(e) => setFilterStatus(e.target.value)}>
+          <option value="all">all</option>
+          <option value="processing">processing</option>
+          <option value="shipped">shipped</option>
+          <option value="returned">returned</option>
+          <option value="restocked">restocked</option>
+        </select>
+      </label>
+      {filterOrders.map((order) => (
         <PreviousOrdersCard order={order} key={order.id} />
       ))}
     </div>
