00a955135fc4a9abe08f501ebb8484b98eab7282
Zach <shaw.zacht@gmail.com>
2022-11-03 15:17:29 -0700

    cleaned up unneeded method and serialized attribute

--- app/models/shopping_session.rb
+++ app/models/shopping_session.rb
@@ -1,15 +1,5 @@
 class ShoppingSession < ApplicationRecord
 
-    # validates :status, uniqueness: true, if: :status_active?
-
-    # def status_active?
-    #     status == "active"
-    # end
-
-    def total_price
-        self.cart_items.sum {|item| item.quantity * item.toy.purchase_price}
-    end
-
     def total_items
         self.cart_items.sum {|item| item.quantity}
     end
--- app/serializers/shopping_session_serializer.rb
+++ app/serializers/shopping_session_serializer.rb
@@ -1,5 +1,5 @@
 class ShoppingSessionSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :status, :total_price, :total_items, :cart_items, :when_created, :last_update
+  attributes :id, :user_id, :status, :total_items, :cart_items, :when_created, :last_update
 
   has_many :cart_items
   has_one :user, serializer: ShoppingSessionUserSerializer
