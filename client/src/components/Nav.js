967ea34456c967c41fbdea9f7af3f4230f2b269c
Zach <shaw.zacht@gmail.com>
2023-04-22 22:57:45 -0700

    adding mobile nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,15 +1,17 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context/user";
 import { CartContext } from "../context/cart";
 import { PreviousOrdersContext } from "../context/previousOrders";
 import "../App.css";
+import MobileNav from "./MobileNav";
 
 function Nav() {
   const { user, setUser } = useContext(UserContext);
   const { setCart } = useContext(CartContext);
   const { setPreviousOrders } = useContext(PreviousOrdersContext);
   const navigate = useNavigate();
+  const [ openNav, setOpenNav ] = useState(false)
 
   function handleLogout() {
     fetch("/logout", { method: "DELETE" }).then((res) => {
@@ -22,31 +24,39 @@ function Nav() {
     });
   }
 
+  console.log(openNav)
+
   if (user) {
     return (
       <div id="nav">
+        <button onClick={() => setOpenNav(!openNav)} className="toggle-button">
+          <span className="toggle-button__bar"></span>
+          <span className="toggle-button__bar"></span>
+          <span className="toggle-button__bar"></span>
+        </button>
+        {openNav ? <MobileNav /> : null}
         {user.is_admin ? (
-        <NavLink to="manage_users" className="nav-link">
+        <NavLink to="manage_users" className="main__nav-link">
           Manage Users
         </NavLink>
         ) : null}
-        <NavLink to="view_toys" className="nav-link">
+        <NavLink to="view_toys" className="main__nav-link">
           View Toys
         </NavLink>
         {user.is_admin ? (
-          <NavLink to="add_toy" className="nav-link">
+          <NavLink to="add_toy" className="main__nav-link">
             Add Toy
           </NavLink>
         ) : null}
         {user.is_admin ? (
-          <NavLink to="view_orders" className="nav-link">
+          <NavLink to="view_orders" className="main__nav-link">
             View Orders
           </NavLink>
         ) : null}
-        <NavLink to={`user_profiles/${user.id}`} className="nav-link">
+        <NavLink to={`user_profiles/${user.id}`} className="main__nav-link">
           My Account
         </NavLink>
-        <button onClick={handleLogout} className="nav-link">
+        <button onClick={handleLogout} className="main__nav-logout">
           Logout
         </button>
       </div>
@@ -54,10 +64,15 @@ function Nav() {
   } else {
     return (
       <div id="nav">
-        <NavLink to="/view_toys" className="nav-link">
+        <button class="toggle-button">
+          <span class="toggle-button__bar"></span>
+          <span class="toggle-button__bar"></span>
+          <span class="toggle-button__bar"></span>
+        </button>
+        <NavLink to="/view_toys" className="main__nav-link">
           View Toys
         </NavLink>
-        <NavLink to="/user_login" className="nav-link">
+        <NavLink to="/user_login" className="main__nav-link">
           Login
         </NavLink>
       </div>
