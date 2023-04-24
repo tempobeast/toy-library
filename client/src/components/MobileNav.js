0b34d47031528827edfdbf1e75cf9c92f5069348
Zach <shaw.zacht@gmail.com>
2023-04-23 17:34:51 -0700

    classNames for mobile-first design

--- client/src/components/MobileNav.js
+++ client/src/components/MobileNav.js
@@ -5,7 +5,7 @@ import { CartContext } from "../context/cart";
 import { PreviousOrdersContext } from "../context/previousOrders";
 import "../App.css";
 
-function MobileNav() {
+function MobileNav({openNav, setOpenNav}) {
   const { user, setUser } = useContext(UserContext);
   const { setCart } = useContext(CartContext);
   const { setPreviousOrders } = useContext(PreviousOrdersContext);
@@ -17,6 +17,7 @@ function MobileNav() {
         setUser(null);
         setCart(null);
         setPreviousOrders([]);
+        setOpenNav(!openNav)
         navigate("/");
       }
     });
@@ -24,46 +25,41 @@ function MobileNav() {
 
   if (user) {
     return (
-      <div id="nav">
-         <button className="toggle-button">
-                <span className="toggle-button__bar"></span>
-                <span className="toggle-button__bar"></span>
-                <span className="toggle-button__bar"></span>
-            </button>
+      <div id="mobile-nav">
         {user.is_admin ? (
-        <NavLink to="manage_users" className="mobile__nav-link ">
+        <NavLink to="manage_users" className="mobile__nav-link" onClick={() => setOpenNav(!openNav)}>
           Manage Users
         </NavLink>
         ) : null}
-        <NavLink to="view_toys" className="mobile__nav-link ">
+        <NavLink to="view_toys" className="mobile__nav-link" onClick={() => setOpenNav(!openNav)}>
           View Toys
         </NavLink>
         {user.is_admin ? (
-          <NavLink to="add_toy" className="moblie__nav-link ">
+          <NavLink to="add_toy" className="mobile__nav-link" onClick={() => setOpenNav(!openNav)}>
             Add Toy
           </NavLink>
         ) : null}
         {user.is_admin ? (
-          <NavLink to="view_orders" className="mobile__nav-link ">
+          <NavLink to="view_orders" className="mobile__nav-link" onClick={() => setOpenNav(!openNav)}>
             View Orders
           </NavLink>
         ) : null}
-        <NavLink to={`user_profiles/${user.id}`} className="mobile__nav-link ">
+        <NavLink to={`user_profiles/${user.id}`} className="mobile__nav-link" onClick={() => setOpenNav(!openNav)}>
           My Account
         </NavLink>
-        <button onClick={handleLogout} className="mobile__nav-logout">
+        <button onClick={handleLogout} className="mobile__nav-logout" >
           Logout
         </button>
       </div>
     );
   } else {
     return (
-      <div id="nav">
-        <button className="toggle-button">
+      <div id="mobile-nav">
+        {/* <button className="toggle-button">
           <span className="toggle-button__bar"></span>
           <span className="toggle-button__bar"></span>
           <span className="toggle-button__bar"></span>
-        </button>
+        </button> */}
         <NavLink to="/view_toys" className="mobile__nav-link">
           View Toys
         </NavLink>
